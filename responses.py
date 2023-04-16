552dd17bb7ef3113a610ca61f5b9fabd9de3a2ad
Katarzyna Michalska <92379328+katarzynamichalskaa@users.noreply.github.com>
2023-04-16 14:07:06 +0200

    Update responses.py

--- responses.py
+++ responses.py
@@ -6,7 +6,5 @@ def handle_response(message: str) -> str:
     p_message = message.lower()
 
     if p_message == '!delay':
-        if TrainDelays() == 1:
-            return 'There are no delays'
-        else:
-            return 'There are delays'
+        return TrainDelays()
+
