ee36c29194eb90311df42c3053405a6d0d8aae26
Katarzyna Michalska <92379328+katarzynamichalskaa@users.noreply.github.com>
2023-04-16 14:07:39 +0200

    Update train_delay_function.py

--- train_delay_function.py
+++ train_delay_function.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import time
+
 
 def TrainDelays():
 
@@ -22,35 +24,45 @@ def TrainDelays():
 
     #INPUT STATION
     start_station = driver.find_element(By.ID, 'from-station')
-    start_station.send_keys("POZNAŃ GŁÓWNY")
+    start_station.send_keys("KRAKÓW GŁÓWNY")
 
     #DESTINATION STATION
     destination_station = driver.find_element(By.ID, 'to-station')
-    destination_station.send_keys("WRONKI")
+    destination_station.send_keys("GDAŃSK GŁÓWNY")
 
     #SEARCH BUTTON
     search_button = driver.find_element(By.ID,'singlebutton')
     search_button.click()
 
     #DELAY CHECKING
+    time.sleep(3)
 
+    on_schedule_train = driver.find_elements(By.CLASS_NAME, 'clear-lowres')
     delayed_train = driver.find_elements(By.CLASS_NAME, 'prognosis')
 
     delayed_train_list = []
+    on_schedule_train_list = []
 
-    for train in delayed_train:
-        if train.find_elements(By.TAG_NAME, "img"):
+    for i, train in enumerate(delayed_train):
+        delayed_train_list.append('0')
+        if train.find_elements(By.CSS_SELECTOR, "span[class='rtinfo']"):
+            print(f"Train with delay found at index {i}")
             delayed_train_list.append(train.text)
 
+    for train in on_schedule_train:
+        if train.find_elements(By.CSS_SELECTOR, "span[class='wcag-hide']"):
+            on_schedule_train_list.append(train.text.strip().replace('\n', '').strip().replace('PRZYJAZD', '').strip().replace('ODJAZD', ''))
+
+    result_delayed = []
+    result_on_schedule_train_list = []
+
+    for i in range(0, len(delayed_train_list)-1, 2):
+        result_delayed.append((delayed_train_list[i], delayed_train_list[i+1]))
+        result_on_schedule_train_list.append((on_schedule_train_list[i], on_schedule_train_list[i + 1]))
+
+    last_list = list(zip(result_delayed, result_on_schedule_train_list))
+    print(last_list)
     driver.quit()
 
-    empty_list_check = 0
+    return last_list
 
-    if not delayed_train_list:
-         print('There are no delays')
-         empty_list_check = 1
-
-    else:
-         print('There are some delays:', delayed_train_list)
-
-    return empty_list_check
