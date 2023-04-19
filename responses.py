edb5d6460082f69df8474f0935cd8f075c673dc3
Katarzyna Michalska <92379328+katarzynamichalskaa@users.noreply.github.com>
2023-04-19 22:06:29 +0200

    Update responses.py

--- responses.py
+++ responses.py
@@ -12,12 +12,12 @@ def handle_response(message: str) -> str:
 
     if p_message == '!help':
         return "Default start station is WRONKI, default destination station is POZNAŃ GŁÓWNY, if you would like to check delays between those, you can use just '!delays'. " \
-               "To change start and destination stations use '!delays FROM_STATION TO_STATION'"
+               "To change start and destination stations use '!delays FROM_STATION TO_STATION'. If your choosen station is a two-part word, please enter it like this: 'POZNAŃ_GŁÓWN'"
 
     FROM, TO = p_message.split()[1], p_message.split()[2]
 
     if p_message == "!delay" + ' ' + FROM + ' ' + TO:
-        return TrainDelays(FROM, TO)
+        return TrainDelays(FROM.strip().replace('_', ' '), TO.strip().replace('_', ' '))
     else:
         return None
 
